0fdcda4e887f8b519c14662e7f985d6b85a64c8a
Aleksandr Kostiuk <kosskill2011@gmail.com>
2018-09-21 14:12:29 +0200

    plik_JS

---
+++ main.js
@@ -0,0 +1,11 @@
+var a = "Uwielbiam JavaScript";
+var b = "Jestem świetnym programistą";
+function str(a, b) {
+    if (a < b) {
+        return (a);
+    } else {
+        return (b);
+    }
+    
+}
+console.log(str(a, b));
